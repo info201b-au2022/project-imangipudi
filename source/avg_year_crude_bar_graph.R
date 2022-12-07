604f58ee4c4d4de831f1813ba534aa2028003ee5
ayaanrahim <ayaanarahim@gmail.com>
2022-12-07 13:32:06 -0800

    Changed dataset for avg_year_crude_bar_graph.R

--- source/avg_year_crude_bar_graph.R
+++ source/avg_year_crude_bar_graph.R
@@ -6,7 +6,7 @@ library(dplyr)
 library(stringr)
 
 crude_rates <- read.csv(
-  file = "https://raw.githubusercontent.com/info201b-au2022/project-imangipudi/main/data/ratescrude.csv",
+  file = "https://raw.githubusercontent.com/info201b-au2022/project-imangipudi/main/data/crude_rates.csv",
   stringsAsFactors = FALSE
   )
 View(crude_rates)
@@ -16,37 +16,9 @@ get_rates <- function(year){
   return(new)
 }
 
-new_crude_rates <- crude_rates %>%
-  mutate("x2019" = get_rates(X2019),
-         "x2018" = get_rates(crude_rates$X2018),
-         "x2017" = get_rates(crude_rates$X2017),
-         "x2016" = get_rates(crude_rates$X2016),
-         "x2015" = get_rates(crude_rates$X2015),
-         "x2014" = get_rates(crude_rates$X2014),
-         "x2013" = get_rates(crude_rates$X2013),
-         "x2012" = get_rates(crude_rates$X2012),
-         "x2011" = get_rates(crude_rates$X2011),
-         "x2010" = get_rates(crude_rates$X2010),
-         "x2009" = get_rates(crude_rates$X2009),
-         "x2008" = get_rates(crude_rates$X2008),
-         "x2007" = get_rates(crude_rates$X2007),
-         "x2006" = get_rates(crude_rates$X2006),
-         "x2005" = get_rates(crude_rates$X2005),
-         "x2004" = get_rates(crude_rates$X2004),
-         "x2003" = get_rates(crude_rates$X2003),
-         "x2002" = get_rates(crude_rates$X2002),
-         "x2001" = get_rates(crude_rates$X2001),
-         "x2000" = get_rates(crude_rates$X2000)
-  )
 
-crude_rates2 <- select(new_crude_rates, -c(X2019, X2018, X2017, X2016, X2015, X2014,
-                                           X2013, X2012, X2011, X2010, X2009, X2008,
-                                           X2007, X2006, X2005, X2004, X2003, X2002,
-                                           X2001, X2000))
 
-View(crude_rates2)
-
-plot_data <- crude_rates2 %>%
+plot_data <- crude_rates %>%
   select(Country, Sex, x2019, x2018, x2017, x2016, x2015, x2014, x2013, x2012, x2011, x2010, x2009, x2008, x2007, x2006, x2005, x2004, x2003, x2002, x2001, x2000) %>%
   filter(Sex == "Both sexes") %>%
   select(x2019, x2018, x2017, x2016, x2015, x2014, x2013, x2012, x2011, x2010, x2009, x2008, x2007, x2006, x2005, x2004, x2003, x2002, x2001, x2000) 
